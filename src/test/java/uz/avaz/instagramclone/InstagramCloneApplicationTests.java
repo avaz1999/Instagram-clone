d8348238bf3d8857b4c52f08495e46beb45658a0
avaz <avazabsamatov7@gmail.com>
2023-04-15 22:30:50 +0500

    first commit

---
+++ src/test/java/uz/avaz/instagramclone/InstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.avaz.instagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
